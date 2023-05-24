fe8df9e0afd024d33737ad4e4bbdfd63efdc4f22
ShalomYasser <134552940+ShalomYasser@users.noreply.github.com>
2023-05-24 16:43:19 -0400

    datos solicitados

---
+++ Clase_12/datos.py
@@ -0,0 +1,9 @@
+#ejemplos de uso github
+print("Ingreso de datos")
+print("----------------")
+nombre = (input("Ingrese su nombre: "))
+edad = (input("Ingrese su edad: "))
+print("-----------------------------")
+print(f"Su nombre es: {nombre}")
+print(f"Su edad es: {edad}")
+print("Programa finalizado...")
